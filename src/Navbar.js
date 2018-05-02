18718ee39e4a78ae717b1882282fdf10e49d58e9
neumann11 <pavlo.vlasyuk@gmail.com>
2018-05-02 17:50:44 +0200

    Add form, save, show/hide form, delete

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './Navbar.css';
 
 class Navbar extends Component {
+    static defaultProps = {
+        onNewRecipe() {}
+    }
+    static propTypes = {
+        onNewRecipe: PropTypes.func
+    }
+
     render() {
         return(
             <header>
                 <h2><a href="#">Recipe App</a></h2>
                 <nav>
-                    <li><a href="#">New Repipe</a></li>
+                    <li><a onClick={this.props.onNewRecipe}>New Repipe</a></li>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Contact Us</a></li>
