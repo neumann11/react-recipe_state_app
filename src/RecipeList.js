5166a8ca09a1bf17339bc0eb20fd6d5fa75121d7
neumann11 <pavlo.vlasyuk@gmail.com>
2018-05-02 15:55:39 +0200

    initialize new project

---
+++ src/RecipeList.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import Recipe from './Recipe';
+import PropTypes from 'prop-types';
+import './RecipeList.css';
+
+class RecipeList extends Component {
+
+
+    static propTypes = {
+        recipes: PropTypes.arrayOf(PropTypes.object).isRequired
+    };
+
+
+    render() {
+        const recipes = this.props.recipes.map((r, index) => (
+            <Recipe key={r.id} {...r} />
+        ));
+
+        return (
+            <div className='recipe-list'>
+                {recipes}
+            </div>
+        )
+
+    }
+}
+
+export default RecipeList;
